5dc0e35000c5de5f816480b22f3a5e56a087aff1
Pedro Marçal Ballesteros <54600506+Netroxx@users.noreply.github.com>
2024-06-04 23:17:56 -0700

    Add files via upload

---
+++ Calculadora Simples.py
@@ -0,0 +1,34 @@
+def adicionar(x, y):
+  return x + y
+
+def subtrair(x, y):
+  return x - y
+
+def multiplicar(x, y):
+  return x * y
+
+def dividir(x, y):
+  return x / y
+
+# Exemplo de uso
+print("Selecione a operação.")
+print("1.Adicionar")
+print("2.Subtrair")
+print("3.Multiplicar")
+print("4.Dividir")
+
+escolha = input("Digite a escolha(1/2/3/4): ")
+
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+
+if escolha == '1':
+  print(num1, "+", num2, "=", adicionar(num1, num2))
+elif escolha == '2':
+  print(num1, "-", num2, "=", subtrair(num1, num2))
+elif escolha == '3':
+  print(num1, "*", num2, "=", multiplicar(num1, num2))
+elif escolha == '4':
+  print(num1, "/", num2, "=", dividir(num1, num2))
+else:
+  print("Entrada inválida")
