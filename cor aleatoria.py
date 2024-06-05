225d7331e51588fc11cd5d0245dcd7a68cd00ad7
Pedro Marçal Ballesteros <54600506+Netroxx@users.noreply.github.com>
2024-06-04 23:24:44 -0700

    Add files via upload

---
+++ cor aleatoria.py
@@ -0,0 +1,4 @@
+import random
+lista = ['vermelho', 'azul', 'verde', 'amarelo', 'roxo']
+escolha = random.choice(lista)
+print(escolha)
---
+++ main.py
@@ -0,0 +1,4 @@
+import random
+lista = ['maçã', 'banana', 'cereja', 'data', 'figo']
+random.shuffle(lista)
+print(lista)
